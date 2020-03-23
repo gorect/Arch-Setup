2160d7fc7907a6852ee8c00af6f151b74dd29dc0
Cameron Gore <gorect@vcu.edu>
2020-03-23 09:38:56 -0400

    Update 01-setup.sh

--- 01-setup.sh
+++ 01-setup.sh
@@ -6,6 +6,9 @@ mount /dev/sda1 /mnt
 echo "nameserver 8.8.4.4" >> /etc/resolv.conf
 echo "nameserver 8.8.8.8" >> /etc/resolv.conf
 
+##Grab the completed pacman mirrorlist
+wget https://raw.github.com/gorect/Arch-setup-guide/master/USmirrorlist
+
 pacstrap /mnt base linux linux-firmware
 
 genfstab -U /mnt >> /mnt/etc/fstab
