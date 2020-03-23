a497906599d163bdde72ee6b36aa18c26d89d32e
Cameron Gore <gorect@vcu.edu>
2020-03-23 09:16:57 -0400

    Update 03-gnome.sh
    Installs gnome defaults and creates/starts the service before rebooting.

--- 03-gnome.sh
+++ 03-gnome.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
+
+##Update system--------------------------------------------------------------------
 sudo pacman -Syyu
+
+##Install Full Gnome with Defaults-------------------------------------------------
 sudo pacman -S gnome --noconfirm
+
+##Enable and Start Gnome severices-------------------------------------------------
 sudo systemctl enable gdm.service
 sudo systemctl start gdm.service
+
 sudo reboot
