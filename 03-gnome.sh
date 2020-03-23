f26db0380f374eda780e69a9d295d6b9736c45ec
Cameron Gore <gorect@vcu.edu>
2020-03-23 13:39:27 -0400

    Update 03-gnome.sh

--- 03-gnome.sh
+++ 03-gnome.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 ##Update system--------------------------------------------------------------------
-sudo pacman -Syyu
+pacman -Syyu
 
 ##Install Full Gnome with Defaults-------------------------------------------------
-sudo pacman -S gnome --noconfirm
+pacman -S gnome --noconfirm
 
 ##Enable and Start Gnome severices-------------------------------------------------
-sudo systemctl enable gdm.service
-sudo systemctl start gdm.service
+systemctl enable gdm.service
+systemctl start gdm.service
 
-sudo reboot
+reboot
