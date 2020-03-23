0552ee87808d6c562cf01e206a3f3d1246c8362b
Cameron Gore <gorect@vcu.edu>
2020-03-23 09:12:51 -0400

    Update 02-chroot.sh

--- 02-chroot.sh
+++ 02-chroot.sh
@@ -1,32 +1,51 @@
 #!/bin/bash
 
-passwd
+##Set Hostname-----------------------------------------------------------
+echo "Please enter a hostname for the new system."
+read -p 'Hostname: ' hostnamevar
+echo ""
 
+##Change Root Password---------------------------------------------------
+echo "Please enter a new Root Password"
+read -sp 'Password: ' passwdvar
+echo "$passwdvar" | passwd --stdin root
+$passwdvar
+
+##Reset /etc/resolv.conf------------Sometimes this does not survive------
+##echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+##echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+
+##Set Locale.gen---------------------------------------------------------
 echo "en_US.UTF-8 UTF-8" >> /etc/locale.gen
-
 locale-gen
 ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
 hwclock --systohc
 echo "LANG=en_US.UTF-8" >> /etc/locale.conf
 
-echo "arch" >> /etc/hostname
+##Set Hostname and Hosts File
+echo $hostnamevar >> /etc/hostname
 
 echo "127.0.0.1	 localhost" >> /etc/hosts
 echo "::1			   localhost" >> /etc/hosts
-echo "127.0.1.1	 arch.localdomain arch" >> /etc/hosts
+echo "127.0.1.1	 $hostnamevar.localdomain $hostnamevar" >> /etc/hosts
 
+##Install DHCP Service---------------------------------------------------
 pacman -S dhcp --noconfirm
 pacman -S dhcpcd --noconfirm
 systemctl enable dhcpcd
 
+##Install Sudo-----------------------------------------------------------
 pacman -S sudo --noconfirm
 
+##Import sudoers File
 echo "wheel ALL=(ALL)" >> /etc/sudoers
 
+##Install Grub-----------------------------------------------------------
 pacman -S grub --noconfirm
 grub-install /dev/sda
 grub-mkconfig -o /boot/grub/grub.cfg
 
+##Install XORG and LightDM-----------------------------------------------
 pacman -S xorg --noconfirm
 pacman -S lightdm --noconfirm
 
