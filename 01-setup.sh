bf54bc4c1cb5b5f07440263585c80361b02cecb7
Cameron Gore <gorect@vcu.edu>
2020-03-25 19:05:00 -0400

    Update 01-setup.sh

--- 01-setup.sh
+++ 01-setup.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-##Ceate the File system and mount it----------------------------------------------------
+##Set Timezone
+timedatectl set-ntp true
+timedatectl status
+sleep 3
+
+##Create the File system and mount it----------------------------------------------------
 echo "Make File System"
 mkfs.ext4 -F /dev/sda1
 mount /dev/sda1 /mnt
 
 ##Add DNS Entry to the resolv.conf file-------------------------------------------------
-echo "nameserver 8.8.4.4" >> /etc/resolv.conf
-echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+##echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+##echo "nameserver 8.8.8.8" >> /etc/resolv.conf
 
 ##Grab the completed pacman mirrorlist---------------------------------------------------
 wget https://raw.github.com/gorect/Arch-setup-guide/master/usMirrorlist
